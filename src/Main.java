6396ac8ed674ee8458455c9084a167a4f0ec7c32
Milton Brito <miltonneto0010@gmail.com>
2024-08-24 19:03:41 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import br.com.dio.desafio.dominio.Conteudo;
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -23,8 +21,37 @@ public class Main {
         mentoria.setDescricao("Descrição mentoria Java");
         mentoria.setData(LocalDate.now());
 
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria);
+//        System.out.println(curso1);
+//        System.out.println(curso2);
+//        System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev dev1 = new Dev();
+        dev1.setNome("Milton");
+        dev1.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos " + dev1.getConteudosInscritos());
+        dev1.progredir();
+        System.out.println("_");
+        System.out.println("Conteúdos Incritos " + dev1.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + dev1.getConteudosConcluidos());
+        System.out.println("XP: " + dev1.calcularTotalXp());
+
+        System.out.println("_____________");
+
+        Dev dev2 = new Dev();
+        dev2.setNome("Isa");
+        dev2.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos " + dev2.getConteudosInscritos());
+        dev2.progredir();
+        System.out.println("_");
+        System.out.println("Conteúdos Incritos " + dev2.getConteudosInscritos());
+        System.out.println("Conteúdos Concluidos " + dev2.getConteudosConcluidos());
+        System.out.println("XP: " + dev2.calcularTotalXp());
     }
 }
